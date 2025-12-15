233f6b04210e78316d59875c20428660283bd4bf
sung <sungwonchung3@gmail.com>
2025-12-15 15:54:17 -0800

    register working example

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from planar import PlanarApp
 from app.db.entities import Invoice
 from app.flows.process_invoice import process_invoice
 from app.flows.process_invoice import invoice_agent
+from app.flows.complex_invoice_process import complex_invoice_process
+from app.flows.complex_invoice_process import invoice_extraction_agent
 from app.router import router
 from app.flows.process_invoice import auto_approver
 from dotenv import load_dotenv
@@ -18,7 +20,10 @@ app = (
 )
 
 app.register_rule(auto_approver)
+app.register_workflow(complex_invoice_process)
+app.register_agent(invoice_extraction_agent)
 
 if __name__ == "__main__":
+
     print("Planar app is ready!")
     exit(0)
